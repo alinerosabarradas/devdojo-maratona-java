25d5efc6d9320ab837bbef625238085a173d1939
adrianorabello <adrianor.rabello@hotmail.com>
2025-06-16 14:43:32 -0300

    first commit

---
+++ src/main/java/br/com/aline/java/javaviradojiraya/JavaViradoJirayaApplication.java
@@ -0,0 +1,13 @@
+package br.com.aline.java.javaviradojiraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaViradoJirayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaViradoJirayaApplication.class, args);
+    }
+
+}
