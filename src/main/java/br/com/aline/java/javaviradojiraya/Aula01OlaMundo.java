a3f7240982a4a2ee71a1d4ad3c97504f1ea38ee2
alinerosabarradas <alinerosabarradas@gmail.com>
2025-06-25 15:28:09 -0300

    Aula 01

---
+++ src/main/java/br/com/aline/java/javaviradojiraya/Aula01OlaMundo.java
@@ -0,0 +1,16 @@
+package br.com.aline.java.javaviradojiraya;
+
+   /**
+   * isso é um java doc
+   */
+public class  Aula01OlaMundo {
+    //Isso é um comentário de uma linha
+    /*
+      isso é um comnetário
+      de multíplas
+      linhas
+    */
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}
---
+++ src/main/java/br/com/aline/java/javaviradojiraya/Aula02TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package br.com.aline.java.javaviradojiraya;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        Integer idade2=10;
+        long numeroGrande = 10000;
+        double salarioDouble =2000;
+        float salarioFloat =2500;
+        byte idadeByte =10;
+        short idadaeShort =10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 87;
+
+        System.out.println("A idade é "+ idade+" anos");
+        System.out.println(falso);
+        System.out.println("char"+caractere);
+
+    }
+}
