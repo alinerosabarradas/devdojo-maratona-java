e132d13876e541e116b16b0f27c113540429fff9
Aline Rosa <alinerosabarradas@gmail.com>
2025-07-01 15:24:04 -0300

    public static void main(String[] args) {

---
+++ src/main/java/br/com/aline/java/javaviradojiraya/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,16 @@
+package br.com.aline.java.javaviradojiraya;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        // Imprima os primeiros 5 números de um dado valor. Por exemplo: 50
+        int valorMax = 50;
+        for (int i = 0; i <= valorMax; i++){
+            if (i > 25) {
+                break;
+            }
+                System.out.println(i);
+            }
+        }
+
+    }
+
