38dcda2d715281c42898dd14653ff4b1c9211989
Aline Rosa <alinerosabarradas@gmail.com>
2025-08-01 16:01:15 -0300

    Objetos construtores

---
+++ src/main/java/br/com/aline/javaviradojiraya/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package br.com.aline.javaviradojiraya.javacore.Dconstrutores.test;
+
+import br.com.aline.javaviradojiraya.javacore.Dconstrutores.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Clara", "TV", 12,"Ação", "Novo");
+        anime.imprime();
+
+    }
+}
