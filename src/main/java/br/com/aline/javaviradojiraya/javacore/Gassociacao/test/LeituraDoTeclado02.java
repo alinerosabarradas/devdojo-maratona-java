93e79878e5209fc9831ea8841b053375deaade9d
Aline Rosa <alinerosabarradas@gmail.com>
2025-08-08 15:02:30 -0300

    Scanner

---
+++ src/main/java/br/com/aline/javaviradojiraya/javacore/Gassociacao/test/LeituraDoTeclado02.java
@@ -0,0 +1,19 @@
+package br.com.aline.javaviradojiraya.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTeclado02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("O grande software de previsão do futuro");
+        System.out.println("Digite sua pergunta e eu reponderei sim ou não");
+        String pergunta = input.nextLine();
+        if(pergunta.charAt(0) == 'S'){
+            System.out.println("SIM");
+        }else{
+            System.out.println("NAO");
+        }
+
+
+    }
+}
